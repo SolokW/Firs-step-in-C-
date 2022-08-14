50c5fd7fca14ea662907e061f7c05c0f9f99d5f3
Alexey <a@a.arbuz>
2022-08-14 23:34:50 +0300

    Task 5 (if and else) and Task 6 Giri

---
+++ Lections/Lection1/Task5_a_OR_b/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+// Есть проблема, связанная с тем, что "Маша", можно написать иначе и код работать не будет.
+// Чтобы это пофиксить, можно написать оператор "ToLower" в строке "if" после "username"
+// Не забыть поставить "." Между ними!!!
