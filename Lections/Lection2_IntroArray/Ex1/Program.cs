f4a2c529af6c09c089832e4e4fc4572569027cba
Alexey <a@a.arbuz>
2022-08-16 12:47:03 +0300

    Lection 2. Array

---
+++ Lections/Lection2_IntroArray/Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Array
+
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+int[] array = { 1, 21, 34, 4, 45, 16, 77, 28, 9 };
+
+array[0] = 12;
+Console.WriteLine(array[0]);
+
+// int max = Max(
+//     Max(array[0], array[1], array[2]),
+//     Max(array[3], array[4], array[5]),
+//     Max(array[6], array[7], array[8])
+// );
+// Console.WriteLine(max);
---
+++ Lections/Lection2_IntroArray/Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти элемент массива, равный find. Массив состоит из n элементов
+
+int[] array = { 1, 4, 78, 62, 2, 32, 79, 14};
+
+int n = array.Length;
+int find = 62;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        
+    }
+    index++;
+}
